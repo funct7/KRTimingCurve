13954ddaf692fff09c7472406b22b71c37f612e1
Joshua Park <wmpark@knowre.com>
2020-05-25 13:24:19 +0900

    Initial Commit

---
+++ KRTimingCurve/KRTimingCurve.h
@@ -0,0 +1,19 @@
+//
+//  KRTimingCurve.h
+//  KRTimingCurve
+//
+//  Created by Joshua Park on 2020/05/25.
+//  Copyright © 2020 Knowre. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KRTimingCurve.
+FOUNDATION_EXPORT double KRTimingCurveVersionNumber;
+
+//! Project version string for KRTimingCurve.
+FOUNDATION_EXPORT const unsigned char KRTimingCurveVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KRTimingCurve/PublicHeader.h>
+
+
